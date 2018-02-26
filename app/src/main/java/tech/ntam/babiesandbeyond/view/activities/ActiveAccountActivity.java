4fb47f8b7286745d921272c4b87ec69e3b030fce
bassiouny <ahmedbassiouny26@gmail.com>
2018-02-26 14:48:01 +0200

    verify email string

--- app/src/main/java/tech/ntam/babiesandbeyond/view/activities/ActiveAccountActivity.java
+++ app/src/main/java/tech/ntam/babiesandbeyond/view/activities/ActiveAccountActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import tech.ntam.babiesandbeyond.R;
@@ -18,12 +19,15 @@ import tech.ntam.mylibrary.apiCongif.BaseResponseInterface;
 public class ActiveAccountActivity extends AppCompatActivity {
 
     private TextInputEditText etActiveAccount;
+    private TextView hint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_account);
         etActiveAccount = findViewById(R.id.et_active_account);
+        hint = findViewById(R.id.hint);
+        hint.setText(getString(R.string.enter_code)+" "+UserSharedPref.getEmail(this));
         findViewById(R.id.btn_send).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
